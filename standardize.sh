edc3b40d83bfab25daab45e46a1d4b2bfb6b50ff
Aidan Harbison <aidan_harbison@brown.edu>
2024-06-18 13:13:11 +0200

    made standardize just for standardizing

--- standardize.sh
+++ standardize.sh
@@ -1,9 +1,3 @@
 #! /bin/bash
 
-# cd ..
-
-# git clone https://github.com/TurkuNLP/CORE-corpus.git
-# git clone https://github.com/TurkuNLP/Multilingual-register-corpora.git
-# # git clone FinCORE
-
 source utils_CORE/standardize_core.sh
