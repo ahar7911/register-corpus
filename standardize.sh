b341fda4b5b17a2fb050327cdffb32a409614740
Aidan Harbison <aidan_harbison@brown.edu>
2024-06-19 22:35:35 +0200

    added train test splitting in corpus folder

--- standardize.sh
+++ standardize.sh
@@ -3,5 +3,14 @@
 module load python/python-3.11.3
 source /home2020/home/lilpa/harbison/experiences/env/bin/activate
 
+mkdir -p corpus/train
+mkdir -p corpus/test
+mkdir -p summaries
+
 source utils_CORE/standardize_core.sh
+
 python analyze_dist.py
+
+rm corpus/train/*
+rm corpus/test/*
+python train_test_split.py
---
+++ train_test_split.py
@@ -0,0 +1,27 @@
+import glob
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+filepaths = glob.glob("corpus/*.tsv")
+
+for filepath in filepaths:
+    lang = filepath[7:-4]
+    dataset = pd.read_csv(filepath, sep='\t')
+
+    X = dataset.iloc[:,1].tolist()
+    y = dataset.iloc[:,0].tolist()
+
+    if len(X) > 500:
+        test_size = 0.2
+    else:
+        test_size = 0.5
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
+
+    train_df = pd.DataFrame(zip(y_train, X_train))
+    train_df.to_csv(f"corpus/train/{lang}.tsv", sep="\t", header=False, index=False) 
+    test_df = pd.DataFrame(zip(y_test, X_test))
+    test_df.to_csv(f"corpus/test/{lang}.tsv", sep="\t", header=False, index=False) 
+
+    print(f"completed {lang} train test split")
+
+print("completed train test splits")
