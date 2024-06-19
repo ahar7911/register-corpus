05a78154d14804da4feebca26d32bce8800ee6bd
Aidan Harbison <aidan_harbison@brown.edu>
2024-06-19 22:55:37 +0200

    moved all script into standardize.sh, more informative print statements

--- standardize.sh
+++ standardize.sh
@@ -6,11 +6,23 @@ source /home2020/home/lilpa/harbison/experiences/env/bin/activate
 mkdir -p corpus/train
 mkdir -p corpus/test
 mkdir -p summaries
+echo "directories made, or already exist"
 
-source utils_CORE/standardize_core.sh
+rm -f corpus/*.tsv
+echo "removed all pre-existing corpus data"
+
+langs=("en" "fi" "fr" "sv" "multi")
+for lang in "${langs[@]}"
+do
+    echo "standardizing CORE $lang"
+    python utils_CORE/standardize_core.py --lang $lang
+    echo "CORE $lang completed standardization"
+done
+echo "CORE completed standardization"
 
 python analyze_dist.py
 
-rm corpus/train/*
-rm corpus/test/*
+rm -f corpus/train/*
+rm -f corpus/test/*
+echo "cleared train and test folders"
 python train_test_split.py
