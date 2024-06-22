763115b21096c0154733d9cfc80a9c99ffc5f67e
Aidan Harbison <aidan_harbison@brown.edu>
2024-06-22 05:06:07 +0200

    train test split much smaller for enormous datasets (aka en)

--- train_test_split.py
+++ train_test_split.py
@@ -11,11 +11,15 @@ for filepath in filepaths:
     X = dataset.iloc[:,1].tolist()
     y = dataset.iloc[:,0].tolist()
 
-    if len(X) > 500:
+    train_size = None
+    if len(X) > 10000:
+        train_size = 8000
+        test_size = 2000
+    elif len(X) > 500:
         test_size = 0.2
     else:
         test_size = 0.5
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=train_size, test_size=test_size, random_state=42)
 
     train_df = pd.DataFrame(zip(y_train, X_train))
     train_df.to_csv(f"corpus/train/{lang}.tsv", sep="\t", header=False, index=False) 
