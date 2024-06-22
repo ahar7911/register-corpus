060df6fb6720997c6e2401784db23fb6bc4399c5
Aidan Harbison <aidan_harbison@brown.edu>
2024-06-22 05:20:47 +0200

    more informative print statements

--- train_test_split.py
+++ train_test_split.py
@@ -6,6 +6,7 @@ filepaths = glob.glob("corpus/*.tsv")
 
 for filepath in filepaths:
     lang = filepath[7:-4]
+    print(f"starting {lang} train test split")
     dataset = pd.read_csv(filepath, sep='\t')
 
     X = dataset.iloc[:,1].tolist()
@@ -20,12 +21,13 @@ for filepath in filepaths:
     else:
         test_size = 0.5
     X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=train_size, test_size=test_size, random_state=42)
+    print(f"split {len(X_train)} train, {len(X_test)} test")
 
     train_df = pd.DataFrame(zip(y_train, X_train))
     train_df.to_csv(f"corpus/train/{lang}.tsv", sep="\t", header=False, index=False) 
     test_df = pd.DataFrame(zip(y_test, X_test))
     test_df.to_csv(f"corpus/test/{lang}.tsv", sep="\t", header=False, index=False) 
 
-    print(f"completed {lang} train test split")
+    print(f"completed {lang} train test split\n")
 
 print("completed train test splits")
