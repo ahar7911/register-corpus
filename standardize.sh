a796a4da7daaf60d05a4bfffe99cc2fa975c6c1e
Aidan Harbison <aidan_harbison@brown.edu>
2024-07-03 17:47:21 +0200

    add alsatian to whole script

--- standardize.sh
+++ standardize.sh
@@ -14,6 +14,10 @@ echo "removed all pre-existing corpus data"
 source utils_core/standardize_core.sh
 
 echo
+python utils_alsacien/standardize_alsacien.py
+echo "completed alsatian standardization"
+
+echo
 python analyze_dist.py
 
 echo
