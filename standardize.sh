2e93d2e23b822a8041f3c92d5c528b422a3b75b7
Aidan Harbison <aidan_harbison@brown.edu>
2024-07-23 19:05:36 +0200

    add print statements for each part of standardize

--- standardize.sh
+++ standardize.sh
@@ -2,6 +2,7 @@
 
 source load_env.sh
 
+echo "START STANDARDIZATION"
 rm -f -r corpus
 mkdir corpus
 echo "created directories and/or removed all pre-existing corpus data"
@@ -17,8 +18,10 @@ python utils_alsatian/standardize_alsatian.py
 
 # corpus analysis
 echo
+echo "START CORPUS ANALYSIS"
 source analyze_dist.sh
 
 # train test split
 echo
+echo "START TRAIN TEST SPLIT"
 source train_test_split.sh
