29e9d38723ef875ec849582340bc8c37d91059fd
Aidan Harbison <aidan_harbison@brown.edu>
2024-07-23 18:13:13 +0200

    moved everything into main methods

--- analyze_dist.py
+++ analyze_dist.py
@@ -4,40 +4,44 @@ import json
 from pathlib import Path
 from collections import Counter
 
-# https://stackoverflow.com/questions/15063936/csv-error-field-larger-than-field-limit-131072
-maxInt = sys.maxsize
-while True: # decrease the maxInt value by factor 10 as long as the OverflowError occurs
-    try:
-        csv.field_size_limit(maxInt)
-        break
-    except OverflowError:
-        maxInt = int(maxInt/10)
+def main():
+    # https://stackoverflow.com/questions/15063936/csv-error-field-larger-than-field-limit-131072
+    maxInt = sys.maxsize
+    while True: # decrease the maxInt value by factor 10 as long as the OverflowError occurs
+        try:
+            csv.field_size_limit(maxInt)
+            break
+        except OverflowError:
+            maxInt = int(maxInt/10)
 
-corpus_dir = Path("corpus")
-filepaths = corpus_dir.glob("*.tsv")
+    corpus_dir = Path("corpus")
+    filepaths = corpus_dir.glob("*.tsv")
 
-for filepath in filepaths:
-    lang = filepath.stem
-    print(f"start analysis of {lang}")
+    for filepath in filepaths:
+        lang = filepath.stem
+        print(f"start analysis of {lang}")
 
-    counter = Counter()
-    with open(filepath, "rt", encoding="utf-8") as src_file:
-        src_reader = csv.reader(src_file, delimiter="\t", quoting=csv.QUOTE_NONE)
-        for row in src_reader:
-            register, text = row[0], row[-1]
-            counter[register] += 1
+        counter = Counter()
+        with open(filepath, "rt", encoding="utf-8") as src_file:
+            src_reader = csv.reader(src_file, delimiter="\t", quoting=csv.QUOTE_NONE)
+            for row in src_reader:
+                register, text = row[0], row[-1]
+                counter[register] += 1
 
-    counts = dict(counter)
-    total = sum(counter.values())
-    percentages = {k : v / total * 100 for k, v in counts.items()}
-    counts["total"] = total
+        counts = dict(counter)
+        total = sum(counter.values())
+        percentages = {k : v / total * 100 for k, v in counts.items()}
+        counts["total"] = total
 
-    summary_dir = Path("summaries")
-    if summary_dir.exists():
-        with open(summary_dir / f"{lang}.json", "w") as file:
-            json.dump({"counts" : counts, "percentages" : percentages}, file, indent=4)
-    else:
-        print("\nsummary folder does not exist, please run analyze_dist.sh instead")
-        sys.exit(1)
-    
-    print(f"completed analysis of {lang}")
+        summary_dir = Path("summaries")
+        if summary_dir.exists():
+            with open(summary_dir / f"{lang}.json", "w") as file:
+                json.dump({"counts" : counts, "percentages" : percentages}, file, indent=4)
+        else:
+            print("\nsummary folder does not exist, please run analyze_dist.sh instead")
+            sys.exit(1)
+        
+        print(f"completed analysis of {lang}")
+
+if __name__ == "__main__":
+    main()
--- train_test_split.py
+++ train_test_split.py
@@ -3,42 +3,46 @@ from pathlib import Path
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-corpus_dir = Path("corpus") # from running standardize.sh
-filepaths = corpus_dir.glob("*.tsv")
+def main():
+    corpus_dir = Path("corpus") # from running standardize.sh
+    filepaths = corpus_dir.glob("*.tsv")
 
-for filepath in filepaths:
-    lang = filepath.stem
-    print(f"starting {lang} train test split")
+    for filepath in filepaths:
+        lang = filepath.stem
+        print(f"starting {lang} train test split")
 
-    dataset = pd.read_csv(filepath, sep='\t')
-    X = dataset.iloc[:,1].tolist()
-    y = dataset.iloc[:,0].tolist()
+        dataset = pd.read_csv(filepath, sep='\t')
+        X = dataset.iloc[:,1].tolist()
+        y = dataset.iloc[:,0].tolist()
 
-    split_args = {"train_size":None, "test_size":None, "random_state":42, "stratify":y}
-    if len(X) > 10000:
-        split_args["train_size"] = 8000
-        split_args["test_size"] = 2000
-    elif len(X) > 500:
-        split_args["test_size"] = 0.2
-    else:
-        split_args["test_size"] = 0.5
-    
-    try:
-        X_train, X_test, y_train, y_test = train_test_split(X, y, **split_args)
-    except ValueError as e:
-        split_args["stratify"] = None
-        print("original stratified split failed, retrying without stratification")
-        X_train, X_test, y_train, y_test = train_test_split(X, y, **split_args)
+        split_args = {"train_size":None, "test_size":None, "random_state":42, "stratify":y}
+        if len(X) > 10000:
+            split_args["train_size"] = 8000
+            split_args["test_size"] = 2000
+        elif len(X) > 500:
+            split_args["test_size"] = 0.2
+        else:
+            split_args["test_size"] = 0.5
+        
+        try:
+            X_train, X_test, y_train, y_test = train_test_split(X, y, **split_args)
+        except ValueError as e:
+            split_args["stratify"] = None
+            print("original stratified split failed, retrying without stratification")
+            X_train, X_test, y_train, y_test = train_test_split(X, y, **split_args)
 
-    print(f"split {len(X_train)} train, {len(X_test)} test")
+        print(f"split {len(X_train)} train, {len(X_test)} test")
 
-    if (corpus_dir / "train").exists() and (corpus_dir / "test").exists():
-        train_df = pd.DataFrame(zip(y_train, X_train))
-        train_df.to_csv(corpus_dir / "train" / f"{lang}.tsv", sep="\t", header=False, index=False) 
-        test_df = pd.DataFrame(zip(y_test, X_test))
-        test_df.to_csv(corpus_dir / "test" / f"{lang}.tsv", sep="\t", header=False, index=False)
-    else:
-        print("\ntrain and test folders do not exist, please run train_test_split.sh instead")
-        sys.exit(1)
+        if (corpus_dir / "train").exists() and (corpus_dir / "test").exists():
+            train_df = pd.DataFrame(zip(y_train, X_train))
+            train_df.to_csv(corpus_dir / "train" / f"{lang}.tsv", sep="\t", header=False, index=False) 
+            test_df = pd.DataFrame(zip(y_test, X_test))
+            test_df.to_csv(corpus_dir / "test" / f"{lang}.tsv", sep="\t", header=False, index=False)
+        else:
+            print("\ntrain and test folders do not exist, please run train_test_split.sh instead")
+            sys.exit(1)
 
-    print(f"completed {lang} train test split\n")
+        print(f"completed {lang} train test split\n")
+
+if __name__ == "__main__":
+    main()
