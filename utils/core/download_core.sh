36269fc451f27aa2b8b23541314857fa1b904cb1
Aidan Harbison <aidan_harbison@brown.edu>
2024-08-07 05:26:27 +0200

    add more informative messages

--- utils/core/download_core.sh
+++ utils/core/download_core.sh
@@ -3,30 +3,41 @@
 mkdir -p ZIPS
 
 # CORE
+echo "start CORE download"
 curl -L -o ZIPS/CORE_en.tar.gz https://api.github.com/repos/TurkuNLP/CORE-corpus/tarball/ 
+echo "start CORE extraction"
 tar -xvf ZIPS/CORE_en.tar.gz
 echo "CORE download and extraction complete"
+echo
 
 #FreCORE and SweCORE
+echo "start FreCORE/SweCORE download"
 curl -L -o ZIPS/CORE_fr_sw.tar.gz https://api.github.com/repos/TurkuNLP/Multilingual-register-corpora/tarball
+echo "start FreCORE/SweCORE extraction"
 tar -xvf ZIPS/CORE_fr_sw.tar.gz
 echo "FreCORE/SweCORE download and extraction complete"
+echo
 
 # FinCORE
 mkdir FinCORE_full
 cd FinCORE_full
+echo "start FinCORE download"
 curl -L -o dev.tsv https://github.com/TurkuNLP/FinCORE_full/releases/download/v1.0/dev.tsv
 curl -L -o fincore_labels.txt https://github.com/TurkuNLP/FinCORE_full/releases/download/v1.0/fincore_labels.txt
 curl -L -o test.tsv https://github.com/TurkuNLP/FinCORE_full/releases/download/v1.0/test.tsv
 curl -L -o train.tsv https://github.com/TurkuNLP/FinCORE_full/releases/download/v1.0/train.tsv
 cd ..
-echo "FinCORE download and extraction complete"
+echo "FinCORE download complete"
+echo
 
 # multilang CORE
+echo "start multilang CORE download"
 curl -L -o ZIPS/CORE_multi.tar.gz https://api.github.com/repos/TurkuNLP/pytorch-registerlabeling/tarball/
+echo "start multilang CORE extraction"
 tar -xvf ZIPS/CORE_multi.tar.gz --wildcards "*/data"
 echo "multilang CORE download and extraction complete"
 
+echo "renaming folders to remove Github info"
 # renaming Github directories, removes "OWNER-" (here "TurkuNLP-"") and "-#######" (number of the commit ref)
 for dir in */; do #
     if [ -d "$dir" ]; then
@@ -38,6 +49,7 @@ for dir in */; do #
 done
 
 # cleaning up multilang
+echo "clean up multilang (remove en fi fr sv multi, fix ru)"
 rm -r pytorch-registerlabeling/data/en
 rm -r pytorch-registerlabeling/data/fi
 rm -r pytorch-registerlabeling/data/fr
