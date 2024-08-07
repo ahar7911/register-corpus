51b7c5b90ac56e9c8311d802a8cd05899dc6830b
Aidan Harbison <aidan_harbison@brown.edu>
2024-08-07 05:33:44 +0200

    add informative print statements

--- utils/innsbruck/download_innsbruck.sh
+++ utils/innsbruck/download_innsbruck.sh
@@ -4,9 +4,11 @@ mkdir -p ZIPS
 echo "downloading innsbruck zip file"
 wget "https://zenodo.org/records/3457917/files/GermInnC_release23092019.zip" -O ZIPS/innsbruck.zip
 echo "download complete"
+echo
 
 mkdir -p innsbruck
 echo "extracting innsbruck zip file"
 unzip ZIPS/innsbruck.zip -d innsbruck
+echo "extracting GermInnC.zip file"
 unzip -q innsbruck/GermInnC.zip -d innsbruck
 echo "extraction complete"
